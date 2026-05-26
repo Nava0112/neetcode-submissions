d7de209f4a57440daa23bf9ea9ddc27165aac18c
Navarasan N S M <nsmnavarasan@gmail.com>
2026-05-26 11:52:37 +0530

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(int a, int b, vector<vector<char>>& board){
+        char k=board[a][b];
+        for(int i=0; i<9; i++){
+            if(a==i) continue;
+            cout<<"v"<<" ";
+            if(k==board[i][b]) return false;
+        }
+        for(int i=0; i<9; i++){
+            if(b==i) continue;
+            cout<<"h"<<" ";
+            if(k==board[a][i]) return false;
+        }
+        int x=a/3;
+        x=x*3;
+        int y=b/3;
+        y=y*3;
+        for(int i=x; i<x+3; i++){
+            for(int j=y; j<y+3; j++){
+                if(a==i && b==j) continue;
+                cout<<"d"<<" ";
+                if(board[i][j]==k) return false;
+            }
+        }
+        return 1;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j]=='.') continue;
+                if(!isValid(i,j, board)) return 0;
+            }
+        }
+        return 1;
+    }
+};
