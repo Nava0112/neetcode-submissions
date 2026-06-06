1df33a0c27f2706fdaee03684e6bbd87483dbf0e
Navarasan N S M <nsmnavarasan@gmail.com>
2026-06-06 21:27:44 +0530

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* head = dummy;
+        if(!list1) return list2;
+        if(!list2) return list1;
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                dummy->next=list1;
+                dummy=dummy->next;
+                list1=list1->next;
+            }
+            else{
+                dummy->next=list2;
+                dummy=dummy->next;
+                list2=list2->next;
+            }
+        }
+        if(list1) dummy->next=list1;
+        if(list2) dummy->next=list2;
+        return head->next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
+        if(k==0) return nullptr;
+        if(k==1) return lists[0];
+        ListNode* ans = mergeTwoLists(lists[0], lists[1]);
+        for(int i=2; i<k; i++){
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+};
